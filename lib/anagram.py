ac5087eed472a2c8499ea314cd06a7a1476fbf2a
TrungCorp <trungle9995@gmail.com>
2024-10-11 19:06:13 -0400

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word = word
+    
+    def match(self,array):
+        list_obj = []
+        for word_obj in array:
+            if(sorted(word_obj) == sorted(self.word)):
+                list_obj.append(word_obj)
+        return list_obj
+            
